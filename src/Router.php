9b5f2c940794c5195dd8dfe24553177536ae76ec
Pushpendra Singh Thakur <thakurpsr@gmail.com>
2025-01-11 13:23:39 +0530

    showHttpError shows json output on service requests

--- src/Router.php
+++ src/Router.php
@@ -1,25 +1,25 @@
 <?php
-    namespace Bucorel\Waf;
+	namespace Bucorel\Waf;
 
-    class Router{
+	class Router{
 
-        function getRoute() : string {
-            if( isset( $_REQUEST['_route'] ) && $_REQUEST['_route'] != '' ){
-                return $_REQUEST['_route'];
-            }else{
-                return DEFAULT_ROUTE;
-            }
-        }
+		function getRoute() : string {
+			if( isset( $_REQUEST['_route'] ) && $_REQUEST['_route'] != '' ){
+				return $_REQUEST['_route'];
+			}else{
+				return DEFAULT_ROUTE;
+			}
+		}
 
-        public static function showHttpError( int $httpStatusCode, string $message ) : void {
-            if( $_SERVER['REQUEST_METHOD'] != 'GET' ){
-                $jr = new JsonResponse();
-                $jr->showError( $httpStatusCode, $message );
-            }else{
-                http_response_code( $httpStatusCode );
-                echo $message;
-                exit;
-            }
-        }
-    }
+		public static function showHttpError( int $httpStatusCode, string $message, $isService=0 ) : void {
+			if( $_SERVER['REQUEST_METHOD'] != 'GET' || $isService == 1 ){
+				$jr = new JsonResponse();
+				$jr->showError( $httpStatusCode, $message );
+			}else{
+				http_response_code( $httpStatusCode );
+				echo $message;
+				exit;
+			}
+		}
+	}
 ?>
